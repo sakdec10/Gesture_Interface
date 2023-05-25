dd3cea8071d5ff76ecfe9c60b32e6bc536a631d3
Saksham Bansal <bansal.saksham952@gmail.com>
2023-05-25 19:38:49 -0400

    Cleaning up code to make changes for pose detection

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 import mediapipe as mp
 import whiteboard as wh
-import autopy as ap
+# import autopy as ap
 import time
 
 def main():
@@ -45,8 +45,8 @@ def main():
     
 
     cv.namedWindow('Image',cv.WND_PROP_FULLSCREEN)
-    cv.setWindowProperty('Image', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
-    cv.setWindowProperty('Image', cv.WND_PROP_TOPMOST, 1)
+    # cv.setWindowProperty('Image', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+    # cv.setWindowProperty('Image', cv.WND_PROP_TOPMOST, 1)
 
     while(True):
         success, img = cap.read()
@@ -85,33 +85,33 @@ def main():
                     textDisplay = "Live Long and Prosper"
 
             #whiteboard trigger
-            if (fingers == [0, 1, 0 , 0, 1] and wrist[1] > indexFinger[1]) and counter >= WB_DELAY and hands[0]["type"] == "Right":
-                drawPoints.clear()
-                pointNum = -1
-                drawCase = False
-                counter = wh.generateWhiteBoard(cap,detector, WB_DELAY)
+            # if (fingers == [0, 1, 0 , 0, 1] and wrist[1] > indexFinger[1]) and counter >= WB_DELAY and hands[0]["type"] == "Right":
+            #     drawPoints.clear()
+            #     pointNum = -1
+            #     drawCase = False
+            #     counter = wh.generateWhiteBoard(cap,detector, WB_DELAY)
             
             #mouseMove trigger
-            if hands[0]["type"] == "Left":
-                textDisplay = "Mouse Mode"
-                if fingers == [0, 1, 0, 0 ,0] and  wrist[1] > indexFinger[1]:
-                    cv.rectangle(img, (420, 100), (600, 300), red, 2)
-                    xMouse = np.interp(indexFinger[0], (420, 640-100), (0, 1920))
-                    yMouse = np.interp(indexFinger[1], (100, 480-200), (0, 1080))
-                    cv.circle(img, indexFinger, 10, yellow, cv.FILLED)
+            # if hands[0]["type"] == "Left":
+            #     textDisplay = "Mouse Mode"
+            #     if fingers == [0, 1, 0, 0 ,0] and  wrist[1] > indexFinger[1]:
+            #         cv.rectangle(img, (420, 100), (600, 300), red, 2)
+            #         xMouse = np.interp(indexFinger[0], (420, 640-100), (0, 1920))
+            #         yMouse = np.interp(indexFinger[1], (100, 480-200), (0, 1080))
+            #         cv.circle(img, indexFinger, 10, yellow, cv.FILLED)
 
-                    #smoothing the mouse movement
-                    clockX = plockX + (xMouse - plockX) / 5
-                    clockY = plockY + (yMouse - plockY) / 5
+            #         #smoothing the mouse movement
+            #         clockX = plockX + (xMouse - plockX) / 5
+            #         clockY = plockY + (yMouse - plockY) / 5
 
-                    ap.mouse.move(1920-clockX, clockY)
-                    plockX, plockY = clockX, clockY
-                if (fingers == [1, 0, 0, 0 ,0] or fingers == [1, 1, 0, 0 ,0]) and mouseCounter >= WB_DELAY:
-                    mouseCounter = 0
-                    ap.mouse.click()
-                elif (fingers == [0, 0, 0, 0 ,1] or  fingers == [0, 1, 0, 0 ,1]) and mouseCounter >= WB_DELAY:
-                    mouseCounter = 0
-                    ap.mouse.click(ap.mouse.Button.RIGHT)
+            #         ap.mouse.move(1920-clockX, clockY)
+            #         plockX, plockY = clockX, clockY
+            #     if (fingers == [1, 0, 0, 0 ,0] or fingers == [1, 1, 0, 0 ,0]) and mouseCounter >= WB_DELAY:
+            #         mouseCounter = 0
+            #         ap.mouse.click()
+            #     elif (fingers == [0, 0, 0, 0 ,1] or  fingers == [0, 1, 0, 0 ,1]) and mouseCounter >= WB_DELAY:
+            #         mouseCounter = 0
+            #         ap.mouse.click(ap.mouse.Button.RIGHT)
                     
 
             #if all fingers are closed then clear the screen
@@ -155,8 +155,8 @@ def main():
 
         #resizing the window
         img = cv.resize(img, (1920,1080), interpolation = cv.INTER_CUBIC)
-        cv.resizeWindow('Image', 320, 240)
-        cv.moveWindow('Image', 1920-320, 0)
+        cv.resizeWindow('Image', 1024, 720)
+        # cv.moveWindow('Image', 1920-320, 0)
         cv.imshow('Image', img)
 
         #counters for delay for whiteboard and mouse movements
